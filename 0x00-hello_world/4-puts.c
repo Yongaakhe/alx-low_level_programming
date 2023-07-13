7e838d5280aa853b10c86d593c123785bbc33c3f
Yongaakhe <siphe.boyce@gmail.com>
2023-07-13 12:59:27 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main- this is the main function
  * Return: 0 (success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\n"Programming is like building a multilingual puzzle");
 	return(0);
 }
