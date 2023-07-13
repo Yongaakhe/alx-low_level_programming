66ee0e210c1dccce842b0f586d20da7eb9c88c0b
Yongaakhe <siphe.boyce@gmail.com>
2023-07-13 13:54:22 -0700

    putsvi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- this is the main function
- * Results: 0 (success)
- */
+* main- this is the main function
+* Results: 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
