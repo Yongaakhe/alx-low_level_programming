2125d1264dc8300a2f700bfa84fb4c4ee8a4c0cd
Yongaakhe <siphe.boyce@gmail.com>
2023-07-14 09:02:44 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main: Entry point
  *
- * Return: Always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return(1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"59);
+	return (1);
 }
