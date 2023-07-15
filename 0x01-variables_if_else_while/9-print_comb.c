cc3b7496b2c85e5d3a7ddfad95a88685af496028
Yongaakhe <siphe.boyce@gmail.com>
2023-07-15 12:20:13 -0700

    combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
+		putchar(i + '0');
+
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
+		putchar(' ');
 	}
 	return (0);
 }
