f17413a3f378df26a192f0ecac7950c6f4042124
Yongaakhe <siphe.boyce@gmail.com>
2023-07-19 04:03:41 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,10 +11,10 @@ void jack_bauer(void)
 		for (j = 0; j <= 59; j++)
 		{
 			_putchar((i / 10) + '0');
-			putchar(i % 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-			putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
