175475276bd2abe4aa1dfc137944e1827391bc8c
Yongaakhe <siphe.boyce@gmail.com>
2023-07-20 12:56:55 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@ void print_to_98(int n)
 {
 	int i;
 
-		if (n >= 0 &&n <= 98)
+		if (n >= 0 && n <= 98)
 		{
 			for (i = n; i <= 98; i++)
 			{
@@ -28,7 +28,7 @@ void print_to_98(int n)
 			}
 			printf("\n");
 		}
-		else if (n > 98)
+		else if (n >= 98)
 		{
 			for (i = n; i > 98; i--)
 			{
