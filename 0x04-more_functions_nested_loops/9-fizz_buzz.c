71eaa5827399344f57d0ee1d85a69096de362b4a
Yongaakhe <siphe.boyce@gmail.com>
2023-07-21 18:59:07 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * print_triangle - a function that prints a triangle, followed by a new line
- * @size: parameter
- * Return: void
+ * main - a function that prints a triangle, followed by a new line
+ * Return: 0 (success)
  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0) &&((i % 5) == 0)
+			printf('%', "FizzBuzz");
+		else if ((i % 3) == 0)
+			printf('%', "Fizz");
+		else if ((i % 5) == 0)
+			printf('%', "Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
