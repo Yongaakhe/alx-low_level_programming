e71e1ea2b8f7479c23e8fda57541d6fd06f690c9
Yongaakhe <siphe.boyce@gmail.com>
2023-07-21 20:03:18 -0700

    multiplicatio

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * mul - a function that multiplies two integers
- * @a: parameter 1
- * @b: parameter 2
- * Return: a * b
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,25 +2,7 @@
 /**
  * print_diagonal - a function that draws a diagonal line on the terminal
  * @n: function parameter
- * @i: function parameter
- * @j:function parameter
  * Return: void
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	_putchar(' ');
-	if else
-	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
-}
