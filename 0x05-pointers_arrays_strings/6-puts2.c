740016a22037710d9e0c3cb6a3437b5064b964da
Yongaakhe <siphe.boyce@gmail.com>
2023-07-24 19:23:00 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string, from first character
+ * @str: string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+}
