c1c6ddca842c060d5ca0b3e08b473cf0b753c81b
Yongaakhe <siphe.boyce@gmail.com>
2023-07-24 18:12:23 -0700

    reverse print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int count  = 0;
 	int i;
+	int count = 0;
 
 	for (i = 0; s[i] != '\0' ; i++)
 		count++;
-	for (i = count - 1 ; 1 >= 0; i--)
+	for (i = count - 1 ; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
