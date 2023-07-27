2de2d1e61a33a0e095d14402cb7fc5addfe09bbd
Yongaakhe <siphe.boyce@gmail.com>
2023-07-27 15:33:26 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; a[j] != '\0' ; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 void reverse_array(int *a, int n);
 char *leet(char *);
+char *rot13(char *);
 #endif
