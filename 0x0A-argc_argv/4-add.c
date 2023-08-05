295e4d99d74ccd0517d6f88bad6a4b22454107b1
Yongaakhe <siphe.boyce@gmail.com>
2023-08-04 19:51:40 -0700

    add argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main -  adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * @i: first number to be added
+ * @k: second number to be added
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j, sum = 0;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			c = argv[i];
+			for (j = 1; j < strlen(c); j++)
+			{
+				if (c[j] < 48 || c[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(c);
+			c++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
