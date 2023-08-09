ae6860b2944a614f148d21aa4d471d9a6df7db94
Yongaakhe <siphe.boyce@gmail.com>
2023-08-09 01:07:43 -0700

    string conc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *output;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	for (i = 0; s1[i] != '\0' ; i++)
 		s1len++;
 	for (i = 0; s2[i] != '\0' ; i++)
