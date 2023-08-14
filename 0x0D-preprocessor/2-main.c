19fa45d2cd30adb67daa24f0f6e3525a9136c934
Yongaakhe <siphe.boyce@gmail.com>
2023-08-14 16:23:37 -0700

    preprocessor main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * File: 2-main.c
+ * Main: prints the name of the file it was compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
