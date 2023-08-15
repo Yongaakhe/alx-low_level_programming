6e6bf989339990f729a585f21b5b999fd338dc1e
Yongaakhe <siphe.boyce@gmail.com>
2023-08-14 17:21:45 -0700

    Addition

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef ADD_H
+#define ADD_H
+/**
+ * File: 4-sum.h
+ * Description:  computes the sum of the numbers x and y
+ * @x: 1st input number
+ * @y: 2nd input number
+ *
+ */
+
+#define SUM(x, y) (x + y)
+#endif
