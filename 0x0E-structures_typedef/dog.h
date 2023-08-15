e19be38ae229780cacd4ce8384b5a59481720617
Yongaakhe <siphe.boyce@gmail.com>
2023-08-14 19:06:39 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog: Initializes a variable of type struct dog
+ * Descr - Initializes a variable of type struct dog
+ * @name: name of the dog
+ * @owner: name of the person owning the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+#endif
