cb5d42929c0fbd44e637ab0c8e589da6e7ddc254
Yongaakhe <siphe.boyce@gmail.com>
2023-08-24 18:38:43 -0700

    write sentence

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints  sentences before the main
+ * first - prints  a sentence before the main
  * function is executed
  */
 void first(void)
