c434b80c6de1e451fb7cbc398b50ea3cc4d379e2
Yongaakhe <siphe.boyce@gmail.com>
2023-10-03 06:38:59 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,11 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, sum, pow;
+	unsigned int len;
+	size_t i;
+	size_t j;
+	size_t sum;
+	size_t pow;
 	int base;
 
 	base = 2;
@@ -17,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	if ((len == 1 && b[0] == '0') || b[0] == '1')
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
 		return (b[0] - 48);
 	for (i = 0; b[i] != '\0'; i++)
 	{
