083ef5289a5f51b5d48da8291fbec6c5765c9acb
Yongaakhe <siphe.boyce@gmail.com>
2023-10-27 08:11:28 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _pow - function that calculates (base ^ pow)
+ * @base: base of exponent
+ * @pow: power of the exponent
+ *
+ * Return: value of (base ^ pow)
+ */
+unsigned long int _pow(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary -  prints the binary representation of a number.
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int div, check;
+	char flag;
+
+	flag = 0;
+	div = _pow(2, sizeof(unsigned long int) * 7);
+	while (div != 0)
+	{
+		check = n & div;
+		if (check == div)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || div == 1)
+		{
+			_putchar('0');
+		}
+		div >>= 1;
+	}
+}
